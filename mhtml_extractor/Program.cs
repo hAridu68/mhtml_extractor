7033e9dd94332bdc1a8ef2ed26788dedab580ae6
aridu68 <aridu68@hotmail.com>
2022-03-03 17:32:09 +0700

    Add Help

--- mhtml_extractor/Program.cs
+++ mhtml_extractor/Program.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Diagnostics;
 namespace mhtml_extractor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            ItemExtractor ti = new ItemExtractor(@"D:\Shared\n\COBA.mhtml");
+            if (args.Length == 0)
+            {                
+                Console.WriteLine("Usage: {Process.GetCurrentProcess().ProcessName} file.mhtml");
+                Console.WriteLine("-----------");
+                Console.WriteLine($"{"",-2}MHTML (MIME encapsulation of aggregate HTML documents) Extructor Content of MIMEs");
+                return;
+            }
+            ItemExtractor ti = new ItemExtractor(args[0]);
             ti.Extruct();
             Console.ReadLine();
         }
