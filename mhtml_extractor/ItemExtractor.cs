f890ec51aaa245f3f41963976e4f3e6ef4d88d10
aridu68 <aridu68@hotmail.com>
2022-03-05 13:19:09 +0700

    FIX: incorrect boundary as Head Of MIME, create empty file at last file extracting

--- mhtml_extractor/ItemExtractor.cs
+++ mhtml_extractor/ItemExtractor.cs
@@ -88,6 +88,8 @@ namespace mhtml_extractor
                 {
                     boundery = bd.Groups[2].Value;
                     hasBoundery = true;
+                    sr.BaseStream.Position = GetEndContent(sr, boundery);
+                    sr.ReadLine();
                     break;
                 }
             }
@@ -95,110 +97,108 @@ namespace mhtml_extractor
             _HeaderMsg.Invoke(boundery);
             while (!sr.EndOfStream)
             {
-                str0 = sr.ReadLine();
-                if (Regex.IsMatch(str0, boundery))
+                long end_content = GetEndContent(sr, boundery);
+                byte[] buff = new byte[512], buff2 = new byte[512], b64BOut = new byte[3];
+                string filetype = "";
+                do
                 {
-                    long end_content = GetEndContent(sr, boundery);
-                    byte[] buff = new byte[512], buff2 = new byte[512], b64BOut = new byte[3];
-                    string filetype = "";
-                    do
+                    string strx = sr.ReadLine();
+                    if (strx == "\r\n")
                     {
-                        string strx = sr.ReadLine();
-                        if (strx == "\r\n")
-                        {
-                            break;
-                        }
-                        Match mc = Regex.Match(strx, @"^Content-Type:\s*([\w]*)/([\w]*)");
-                        if (mc.Success)
-                        {
-                            filetype = "." + mc.Groups[2];
-                            continue;
-                        }
-                        mc = Regex.Match(strx, @"^Content-Transfer-Encoding:\s*([\S]*)");
-                        if (mc.Success)
-                        {
-                            encoding = mc.Groups[1].Value;
-                            continue;
-                        }
-                    } while (!sr.EndOfStream);
+                        break;
+                    }
+                    Match mc = Regex.Match(strx, @"^Content-Type:\s*([\w]*)/([\w]*)");
+                    if (mc.Success)
+                    {
+                        filetype = "." + mc.Groups[2];
+                        continue;
+                    }
+                    mc = Regex.Match(strx, @"^Content-Transfer-Encoding:\s*([\S]*)");
+                    if (mc.Success)
+                    {
+                        encoding = mc.Groups[1].Value;
+                        continue;
+                    }
+                } while (!sr.EndOfStream);
 
-                    long fsize = end_content - sr.BaseStream.Position;
-                    FileName = GetRandomFileName(filetype);
-                    using (FileStream fs = new FileStream(FileName, FileMode.CreateNew))
+                long fsize = end_content - sr.BaseStream.Position;
+                FileName = GetRandomFileName(filetype);
+                using (FileStream fs = new FileStream(FileName, FileMode.CreateNew))
+                {
+                    _OutputCallback.Invoke(FileName, fsize, filetype);
+                    switch (encoding)
                     {
-                        _OutputCallback.Invoke(FileName, fsize, filetype);
-                        switch (encoding)
-                        {
-                            case "quoted-printable":
-                                StringBuilder sb = new StringBuilder();
-                                while (true)
+                        case "quoted-printable":
+                            StringBuilder sb = new StringBuilder();
+                            while (true)
+                            {
+                                string txt = sr.ReadLine();
+                                if (!(sr.BaseStream.Position < end_content))
+                                    break;
+                                int ln = txt.ToBytes(buff, 0) - 2;
+                                int rLn = 0, wLn = 0;
+                                byte tmp = 0;
+                                while (rLn < ln)
                                 {
-                                    string txt = sr.ReadLine();
-                                    if (!(sr.BaseStream.Position < end_content))
-                                        break;
-                                    int ln = txt.ToBytes(buff, 0) - 2;
-                                    int rLn = 0, wLn = 0;
-                                    byte tmp = 0;
-                                    while (rLn < ln)
+                                    switch (buff[rLn])
                                     {
-                                        switch (buff[rLn])
-                                        {
-                                            case 61:
-                                                if ((rLn + 1) == ln)
-                                                {
-                                                    tmp = buff2[--wLn];
-                                                    break;
-                                                }
-                                                tmp = Hex2Ascii(buff, rLn + 1, 2);
-                                                rLn += 2;
+                                        case 61:
+                                            if ((rLn + 1) == ln)
+                                            {
+                                                tmp = buff2[--wLn];
                                                 break;
-                                            default:
-                                                tmp = buff[rLn];
-                                                break;
-                                        }
-                                        buff2[wLn] = tmp;
-                                        wLn++; rLn++;
+                                            }
+                                            tmp = Hex2Ascii(buff, rLn + 1, 2);
+                                            rLn += 2;
+                                            break;
+                                        default:
+                                            tmp = buff[rLn];
+                                            break;
                                     }
-                                    fs.Write(buff2, 0, wLn);
+                                    buff2[wLn] = tmp;
+                                    wLn++; rLn++;
                                 }
+                                fs.Write(buff2, 0, wLn);
+                            }
+                            fs.Flush();
+                            sr.BaseStream.Position = end_content;
+                            break;
+                        case "base64":
+                            base64 = new FromBase64Transform(FromBase64TransformMode.IgnoreWhiteSpaces);
+                            int inputbytes, byteoffset, iBlockSize = 4;
+                            while (sr.BaseStream.Position < end_content)
+                            {
+                                string txt = sr.ReadLine();
+                                if (txt == "\r\n")
+                                    break;
+                                byteoffset = 0;
+                                inputbytes = txt.Substring(0, txt.Length - 2).ToBytes(buff, 0);
+                                while (inputbytes - byteoffset > iBlockSize)
+                                {
+                                    base64.TransformBlock(buff, byteoffset, 4, b64BOut, 0);
+                                    byteoffset += 4;
+                                    fs.Write(b64BOut, 0, base64.OutputBlockSize);
+                                }
+                                b64BOut = base64.TransformFinalBlock(buff, byteoffset, inputbytes - byteoffset);
+                                fs.Write(b64BOut, 0, b64BOut.Length);
                                 fs.Flush();
-                                sr.BaseStream.Position = end_content;
-                                break;
-                            case "base64":
-                                base64 = new FromBase64Transform(FromBase64TransformMode.IgnoreWhiteSpaces);
-                                int inputbytes, byteoffset, iBlockSize = 4;
-                                while (sr.BaseStream.Position < end_content)
-                                {
-                                    string txt = sr.ReadLine();
-                                    if (txt == "\r\n")
-                                        break;
-                                    byteoffset = 0;
-                                    inputbytes = txt.Substring(0, txt.Length - 2).ToBytes(buff, 0);
-                                    while (inputbytes - byteoffset > iBlockSize)
-                                    {
-                                        base64.TransformBlock(buff, byteoffset, 4, b64BOut, 0);
-                                        byteoffset += 4;
-                                        fs.Write(b64BOut, 0, base64.OutputBlockSize);
-                                    }
-                                    b64BOut = base64.TransformFinalBlock(buff, byteoffset, inputbytes - byteoffset);
-                                    fs.Write(b64BOut, 0, b64BOut.Length);
-                                    fs.Flush();
-                                }
-                                base64.Clear();
-                                break;
-                            default:
-                                while (sr.BaseStream.Position < end_content)
-                                {
-                                    int wz = (int)(end_content - sr.BaseStream.Position);
-                                    wz = wz < buff.Length ? wz : buff.Length;
-                                    sr.BaseStream.Read(buff, 0, wz);
-                                    fs.Write(buff, 0, wz);
-                                    fs.Flush();
-                                }
-                                break;
-                        }
+                            }
+                            base64.Clear();
+                            break;
+                        default:
+                            while (sr.BaseStream.Position < end_content)
+                            {
+                                int wz = (int)(end_content - sr.BaseStream.Position);
+                                wz = wz < buff.Length ? wz : buff.Length;
+                                sr.BaseStream.Read(buff, 0, wz);
+                                fs.Write(buff, 0, wz);
+                                fs.Flush();
+                            }
+                            break;
                     }
                 }
+                sr.BaseStream.Position = end_content;
+                sr.ReadLine();
             }
 
         }
